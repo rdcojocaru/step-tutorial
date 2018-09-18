4a67e5542e54ac9a81b2322ce6f3e7d8feed9f83
Radu Cojocaru <radu.cojocaru@tremend.com>
2018-09-18 11:23:27 +0300

    modify run

--- run.sh
+++ run.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-GIT_MESSAGE = "DEFAULT MESSAGE"
-
-if [ "$WERCKER_TRIGGER_TEST_MESSAGE" ]; then
-	GIT_MESSAGE="$WERCKER_TRIGGER_TEST_MESSAGE"
-fi
-
-generate_post_data()
-{
-  cat <<EOF
-{
-  "message": "$GIT_MESSAGE"
-}
-EOF
-}
-
-echo "JSON: $(generate_post_data)"
+echo "TEST"
 
 success "\nTest triggered successfully."
